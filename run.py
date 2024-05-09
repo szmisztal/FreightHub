adc3a8256e069174f3c6eab514206988a622b9ee
szmisztal <sz.misztal@gmail.com>
2024-05-09 17:06:37 +0200

    register Blueprint for user model

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+from flask import render_template
 from app import app
+
+@app.route("/")
+def home():
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
