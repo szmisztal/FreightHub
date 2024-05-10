fda1851f2b635f99a13780f9a636ed157e701dde
szmisztal <sz.misztal@gmail.com>
2024-05-10 11:54:55 +0200

    add UserMixin to User model

--- app/user/models.py
+++ app/user/models.py
@@ -1,6 +1,7 @@
+from flask_login import UserMixin
 from app import db
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     first_name = db.Column(db.String(20), nullable=False)
