62cecd2e9147d2f5b667485fc39fe0dbe53ece22
szmisztal <sz.misztal@gmail.com>
2024-05-10 11:53:37 +0200

    add login handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 migrate = None
 bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -16,9 +18,18 @@ def create_app():
     db.init_app(app)
     global migrate
     migrate = Migrate(app, db)
+
     bcrypt.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = "user.login"
 
     from app.user import user_bp
     app.register_blueprint(user_bp, url_prefix="/user")
+
+    from app.user.models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
