0bcc1deafabe117ea6b0377166ae46d2cc277066
szmisztal <sz.misztal@gmail.com>
2024-05-10 10:14:59 +0200

    refactor register function to be more modular

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,32 +1,36 @@
 from flask import render_template, redirect, url_for, flash
-from app import app, db
+from . import user_bp
+from app import db, bcrypt
 from app.user.forms import RegistrationForm
 from app.user.models import User
-import bcrypt
 
-@app.route("/register", methods=["GET", "POST"])
+@user_bp.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        existing_user = User.query.filter_by(username=form.username.data).first()
-        if existing_user is None:
-            existing_user = User.query.filter_by(email=form.email.data).first()
-            if existing_user is None:
-                hashed_password = bcrypt.hashpw(form.password.data.encode("utf-8"), bcrypt.gensalt())
-                hashed_password = hashed_password.decode("utf-8")
-                user = User(
-                    username=form.username.data,
-                    first_name=form.first_name.data,
-                    last_name=form.last_name.data,
-                    email=form.email.data,
-                    password_hash=hashed_password,
-                    role=form.role.data
-                )
-                db.session.add(user)
-                db.session.commit()
-                return redirect(url_for("login"))
-            else:
-                flash("Email already registered.")
-        else:
-            flash("Username already taken.")
+        if user_exists(form.username.data, form.email.data):
+            return render_template("register.html", form=form)
+        user = create_user(form)
+        db.session.add(user)
+        db.session.commit()
+        flash("Registration successful, please login.", "success")
+        return redirect(url_for("user.login"))
     return render_template("register.html", form=form)
+
+def user_exists(username, email):
+    existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+    if existing_user:
+        flash("Username or email already registered.", "error")
+        return True
+    return False
+
+def create_user(form):
+    hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+    return User(
+        username=form.username.data,
+        first_name=form.first_name.data,
+        last_name=form.last_name.data,
+        email=form.email.data,
+        password_hash=hashed_password,
+        role=form.role.data
+    )
