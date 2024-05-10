bc65d3492dde52f3f1e66c9a2d57e07ff1be17de
szmisztal <sz.misztal@gmail.com>
2024-05-10 10:13:06 +0200

    refactor creation app with application factory

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,23 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.user import user_bp
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "remember_to_add_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy()
+migrate = None
+bcrypt = Bcrypt()
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "remember_to_add_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-bcrypt = Bcrypt(app)
+    db.init_app(app)
+    global migrate
+    migrate = Migrate(app, db)
+    bcrypt.init_app(app)
 
-app.register_blueprint(user_bp, url_prefix="/user")
+    from app.user import user_bp
+    app.register_blueprint(user_bp, url_prefix="/user")
 
-from app.user import routes
+    return app
--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from flask import render_template
-from app import app
+from app import create_app
+
+app = create_app()
 
 @app.route("/")
 def home():
     return render_template("base.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
