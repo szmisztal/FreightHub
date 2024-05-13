d50e874d745a7e2cf0cd7ff33c60b97e8950f365
szmisztal <sz.misztal@gmail.com>
2024-05-13 13:34:38 +0200

    change columns length

--- app/user/models.py
+++ app/user/models.py
@@ -3,12 +3,12 @@ from app import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    first_name = db.Column(db.String(20), nullable=False)
-    last_name = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(16), unique=True, nullable=False)
+    first_name = db.Column(db.String(16), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(16), nullable=False)
 
     def __repr__(self):
         return f"{self.username} - {self.first_name} {self.last_name} - {self.role}"
