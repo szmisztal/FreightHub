cdf1d0da49dd0fe97ee8212045017fa87e7d4873
szmisztal <sz.misztal@gmail.com>
2024-05-13 13:34:07 +0200

    change fields length

--- app/user/forms.py
+++ app/user/forms.py
@@ -4,12 +4,12 @@ from wtforms.validators import InputRequired, Email, Length, DataRequired
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)])
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=16)])
     first_name = StringField("First name", validators=[InputRequired()])
     last_name = StringField("Last name", validators=[InputRequired()])
     email = StringField("Email", validators=[InputRequired(), Email()])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=8)])
-    role = SelectField("Role", choices=[("logistician", "Logistician"),
+    role = SelectField("Role", choices=[("planner", "Planner"),
                                         ("dispatcher", "Dispatcher"),
                                         ("driver", "Driver")],
                                         validators=[InputRequired()])
