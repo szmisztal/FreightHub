f543aed5464934c51453989571a015ae20d7eef7
szmisztal <sz.misztal@gmail.com>
2024-05-14 13:28:20 +0200

    add form for create Transportation order

--- app/planner/forms.py
+++ app/planner/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import InputRequired, Length
+from wtforms import StringField, IntegerField, SubmitField, SelectField
+from wtforms.validators import InputRequired, Length, DataRequired, NumberRange
+from app.planner.models import Company
+
 
 class CompanyForm(FlaskForm):
     company_name = StringField("Company name", validators=[InputRequired(), Length(max=32)])
@@ -11,3 +13,24 @@ class CompanyForm(FlaskForm):
     street_number = IntegerField("Street number", validators=[InputRequired()])
     phone_number = StringField("Phone number", validators=[InputRequired()])
     submit = SubmitField("Submit")
+
+class TransportationOrderForm(FlaskForm):
+    trailer_type = SelectField("Trailer type", choices=[("Curtain side", "Curtain-side trailer"),
+                                                        ("Refrigerated", "Refrigerated trailer"),
+                                                        ("Tipper", "Tipper trailer"),
+                                                        ("Low loader","Low-loader trailer"),
+                                                        ("Container", "Container trailer"),
+                                                        ("Self-unloading", "Self-unloading trailer"),
+                                                        ("Insulated", "Insulated trailer")],
+                                               validators=[InputRequired()])
+    load_weight = IntegerField("Load weight", validators=[InputRequired(), NumberRange(min=1000,
+                                                                                       max=24000,
+                                                                                       message="Weight must be in range 1000-24000")])
+    loading_place = SelectField("Loading place", choices=[], validators=[DataRequired()])
+    delivery_place = SelectField("Delivery place", choices=[], validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    def __init__(self, *args, **kwargs):
+        super(TransportationOrderForm, self).__init__(*args, **kwargs)
+        self.loading_place.choices = [(c.id, c.company_name) for c in Company.query.all()]
+        self.delivery_place.choices = [(c.id, c.company_name) for c in Company.query.all()]
