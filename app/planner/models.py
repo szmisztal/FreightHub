3184a09c2f3a04fda11f7e0bfabcf55e4dda39af
szmisztal <sz.misztal@gmail.com>
2024-05-14 13:28:41 +0200

    add model for Transportation order

--- app/planner/models.py
+++ app/planner/models.py
@@ -1,3 +1,4 @@
+import datetime
 from app import db
 
 class Company(db.Model):
@@ -17,3 +18,20 @@ class Company(db.Model):
                f"\nPostal code: {self.postal_code}" \
                f"\nStreet: {self.street} {self.street_number}" \
                f"\nPhone number: {self.phone_number}"
+
+class TransportationOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, default = datetime.date.today, nullable=False)
+    trailer_type = db.Column(db.String(16), nullable=False)
+    load_weight = db.Column(db.Integer, nullable=False)
+    loading_place = db.Column(db.Integer, db.ForeignKey("company.id"), nullable=False)
+    delivery_place = db.Column(db.Integer, db.ForeignKey("company.id"), nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        return f"Date: {self.date}" \
+               f"\nTrailer type: {self.trailer_type}" \
+               f"\nLoad weight: {self.load_weight}" \
+               f"\nLoading place: {self.loading_place}" \
+               f"\nDelivery place: {self.delivery_place}" \
+               f"\nCompleted: {self.completed}"
