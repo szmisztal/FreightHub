90ea0d96972193037f4e6d2cc2120de29008d931
szmisztal <sz.misztal@gmail.com>
2024-05-16 14:31:51 +0200

    add driver field to TransportationOrder model

--- app/planner/models.py
+++ app/planner/models.py
@@ -21,12 +21,13 @@ class Company(db.Model):
 
 class TransportationOrder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, default = datetime.date.today, nullable=False)
+    date = db.Column(db.Date, default=datetime.date.today, nullable=False)
     trailer_type = db.Column(db.String(16), nullable=False)
     load_weight = db.Column(db.Integer, nullable=False)
     loading_place = db.Column(db.Integer, db.ForeignKey("company.id"), nullable=False)
     delivery_place = db.Column(db.Integer, db.ForeignKey("company.id"), nullable=False)
-    completed = db.Column(db.Boolean, default=False, nullable=False)
+    driver = db.Column(db.String, db.ForeignKey("user.id"), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f"Date: {self.date}" \
