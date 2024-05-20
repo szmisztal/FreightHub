c59a1a468828fd2a4f5e82ea57ee5d3c4108745b
szmisztal <sz.misztal@gmail.com>
2024-05-20 12:06:46 +0200

    add create logger func to config app logging system

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+import logging
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +10,20 @@ db = SQLAlchemy()
 migrate = None
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+
+def create_logger(app, log_folder, log_file_name):
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+
+    log_path = os.path.join(log_folder, log_file_name)
+
+    handler = logging.FileHandler(log_path)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
 
 def create_app():
     app = Flask(__name__)
@@ -25,13 +41,15 @@ def create_app():
     login_manager.login_view = "user.login"
 
     from app.user import user_bp
-    from app.planner import planner_bp
-    from app.dispatcher import dispatcher_bp
-    from app.driver import driver_bp
-
     app.register_blueprint(user_bp, url_prefix="/user")
+
+    from app.planner import planner_bp
     app.register_blueprint(planner_bp, url_prefix="/planner")
+
+    from app.dispatcher import dispatcher_bp
     app.register_blueprint(dispatcher_bp, url_prefix="/dispatcher")
+
+    from app.driver import driver_bp
     app.register_blueprint(driver_bp, url_prefix="/driver")
 
     from app.common.models import User
@@ -39,5 +57,7 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+
+    create_logger(app, "logs", "app.log")
 
     return app
