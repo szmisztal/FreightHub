fa191e4d1b64f847ddb626c5f92a5498f17e305b
szmisztal <sz.misztal@gmail.com>
2024-05-22 11:52:50 +0200

    create new database to reset

--- migrations/versions/704a42091e74_create_new_db.py
+++ migrations/versions/704a42091e74_create_new_db.py
@@ -1,8 +1,8 @@
-"""delete old db, create new one
+"""create new db
 
-Revision ID: ae78a5999f08
+Revision ID: 704a42091e74
 Revises: 
-Create Date: 2024-05-21 10:50:03.749067
+Create Date: 2024-05-22 10:39:06.404093
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ae78a5999f08'
+revision = '704a42091e74'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -44,7 +44,7 @@ def upgrade():
     op.create_table('transportation_order',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('creation_date', sa.DateTime(), nullable=False),
-    sa.Column('creation_by', sa.Integer(), nullable=False),
+    sa.Column('created_by', sa.Integer(), nullable=False),
     sa.Column('planned_delivery_date', sa.Date(), nullable=False),
     sa.Column('trailer_type', sa.String(length=16), nullable=False),
     sa.Column('load_weight', sa.Integer(), nullable=False),
@@ -52,10 +52,10 @@ def upgrade():
     sa.Column('delivery_place', sa.Integer(), nullable=False),
     sa.Column('driver', sa.Integer(), nullable=True),
     sa.Column('completed', sa.Boolean(), nullable=True),
-    sa.ForeignKeyConstraint(['creation_by'], ['user.id'], ),
-    sa.ForeignKeyConstraint(['delivery_place'], ['company.id'], ),
-    sa.ForeignKeyConstraint(['driver'], ['user.id'], ),
-    sa.ForeignKeyConstraint(['loading_place'], ['company.id'], ),
+    sa.ForeignKeyConstraint(['created_by'], ['user.id'], name='fk_transportation_order_creation_by'),
+    sa.ForeignKeyConstraint(['delivery_place'], ['company.id'], name='fk_transportation_order_delivery_place'),
+    sa.ForeignKeyConstraint(['driver'], ['user.id'], name='fk_transportation_order_driver'),
+    sa.ForeignKeyConstraint(['loading_place'], ['company.id'], name='fk_transportation_order_loading_place'),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
