2788c9fab5cd9195ec66d77e6d2897e3dc7bf3b6
szmisztal <sz.misztal@gmail.com>
2024-05-25 12:01:14 +0200

    add phone_number field in form

--- app/user/forms.py
+++ app/user/forms.py
@@ -7,13 +7,14 @@ class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=16)])
     first_name = StringField("First name", validators=[InputRequired()])
     last_name = StringField("Last name", validators=[InputRequired()])
+    phone_number = StringField("Phone number", validators=[DataRequired()])
     email = StringField("Email", validators=[InputRequired(), Email()])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=8)])
     role = SelectField("Role", choices=[("planner", "Planner"),
                                         ("dispatcher", "Dispatcher"),
                                         ("driver", "Driver")],
                                validators=[InputRequired()])
-    submit = SubmitField("Register")
+    submit = SubmitField("Submit")
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
