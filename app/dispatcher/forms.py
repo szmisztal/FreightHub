cddc9149c3aefe9656697d6680e0a4df27860722
szmisztal <sz.misztal@gmail.com>
2024-05-25 20:00:31 +0200

    add TractorHeadForm and TrailerForm

--- app/dispatcher/forms.py
+++ app/dispatcher/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, SubmitField, StringField
 from wtforms.validators import DataRequired
 from app import db
 from app.common.models import User, TransportationOrder
+
+class TractorHeadForm(FlaskForm):
+    brand = StringField("Brand", validators=[DataRequired()])
+    registration_number = StringField("Registration Number", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class TrailerForm(FlaskForm):
+    type = SelectField("Type", choices=[("Curtain side", "Curtain-side trailer"),
+                                        ("Refrigerated", "Refrigerated trailer"),
+                                        ("Tipper", "Tipper trailer"),
+                                        ("Low loader","Low-loader trailer"),
+                                        ("Container", "Container trailer"),
+                                        ("Self-unloading", "Self-unloading trailer"),
+                                        ("Insulated", "Insulated trailer")],
+                               validators=[DataRequired()])
+    registration_number = StringField("Registration Number", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 class AssignDriverForm(FlaskForm):
     driver = SelectField("Driver", choices=[], validators=[DataRequired()])
