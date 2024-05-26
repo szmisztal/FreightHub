fa2fc6bad36760eff6b6456f73c392e4aed848de
szmisztal <sz.misztal@gmail.com>
2024-05-26 09:32:00 +0200

    move Trailer model from dispatcher models to common

--- app/dispatcher/models.py
+++ app/dispatcher/models.py
@@ -3,15 +3,8 @@ from app import db
 class TractorHead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     brand = db.Column(db.String(16), nullable=False)
-    registration_number = db.Column(db.String(16), nullable=False)
+    registration_number = db.Column(db.String(16), unique=True, nullable=False)
 
     def __repr__(self):
         return f"{self.brand.upper()} {self.registration_number.upper()}"
 
-class Trailer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(16), nullable=False)
-    registration_number = db.Column(db.String(16), nullable=False)
-
-    def __repr__(self):
-        return f"{self.type.upper()} {self.registration_number.upper()}"
