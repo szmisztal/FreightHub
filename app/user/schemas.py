0b8f97b2ffbfcc62724bccf07d5f2bbbed7e38c2
szmisztal <sz.misztal@gmail.com>
2024-05-31 12:54:04 +0200

    add schema for User model

---
+++ app/user/schemas.py
@@ -0,0 +1,15 @@
+from marshmallow import Schema, fields, validate, validates, ValidationError
+
+class UserSchema(Schema):
+    username = fields.Str(required=True, validate=validate.Length(min=8, max=16))
+    first_name = fields.Str(required=True, validate=validate.Length(min=8, max=16))
+    last_name = fields.Str(required=True, validate=validate.Length(min=8, max=16))
+    phone_number = fields.Str(required=True, validate=validate.Length(min=8, max=32))
+    email = fields.Email(required=True)
+    password_hash = fields.Str(required=True, validate=validate.Length(min=8, max=128))
+    role = fields.Str(required=True, validate=validate.OneOf(["planner", "dispatcher", "driver"]))
+
+    @validates("phone_number")
+    def validate_phone_number(self, value):
+        if not value.isdigit() and not (value.startswith('+') and value[1:].isdigit()):
+            raise ValidationError("Phone number must contain only digits and optional leading '+'.")
