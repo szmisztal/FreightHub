13b7118cde8572a69521b596473ba0313266d580
szmisztal <sz.misztal@gmail.com>
2024-05-31 12:55:32 +0200

    change data length in TractorHead.registration_number column

--- app/dispatcher/models.py
+++ app/dispatcher/models.py
@@ -3,8 +3,8 @@ from app import db
 class TractorHead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     brand = db.Column(db.String(16), nullable=False)
-    registration_number = db.Column(db.String(16), unique=True, nullable=False)
+    registration_number = db.Column(db.String(8), unique=True, nullable=False)
 
     def __repr__(self):
-        return f"{self.brand} {self.registration_number.upper()}"
+        return f"{self.brand} {self.registration_number}"
 
