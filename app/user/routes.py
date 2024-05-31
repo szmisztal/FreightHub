c3562b54e90ff245297810613d7e178485ae45c8
szmisztal <sz.misztal@gmail.com>
2024-05-31 12:56:56 +0200

    add schemas validation in User create/update functions

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,41 +1,57 @@
 from flask import render_template, redirect, url_for, flash, request, current_app
 from flask_login import login_user, logout_user, current_user, login_required
 from sqlalchemy.exc import IntegrityError
+from marshmallow import ValidationError
 from app import db, bcrypt
 from app.common.models import User
 from . import user_bp
 from .forms import RegistrationForm, LoginForm
+from .schemas import UserSchema
 
 @user_bp.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    schema = UserSchema()
     if form.validate_on_submit():
+        user_data = {
+            "username": form.username.data,
+            "first_name": form.first_name.data,
+            "last_name": form.last_name.data,
+            "phone_number": form.phone_number.data,
+            "email": form.email.data,
+            "password": form.password.data,
+            "role": form.role.data
+        }
         try:
-            user = create_user(form)
+            result = schema.load(user_data)
+            user = create_user(result)
             db.session.add(user)
             db.session.commit()
             flash("Registration successfully, you can log in.", "success")
             return redirect(url_for("user.login"))
+        except ValidationError as e:
+            current_app.logger.exception(f"Register - validation error: {e}")
+            flash(f"Validation error: {e}", "danger")
         except IntegrityError as e:
             db.session.rollback()
             current_app.logger.exception(f"Registration error: {e}")
-            flash(f"Username or email in used, choose another", "danger")
+            flash("Username or email is already in use, choose another.", "danger")
         except Exception as e:
             db.session.rollback()
             current_app.logger.exception(f"Registration error: {e}")
             flash(f"Error: {e}, try again", "danger")
     return render_template("register.html", form=form, title="Registration")
 
-def create_user(form):
-    hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+def create_user(data):
+    hashed_password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
     return User(
-        username=form.username.data,
-        first_name=form.first_name.data,
-        last_name=form.last_name.data,
-        phone_number=form.phone_number.data,
-        email=form.email.data,
+        username=data["username"],
+        first_name=data["first_name"],
+        last_name=data["last_name"],
+        phone_number=data["phone_number"],
+        email=data["email"],
         password_hash=hashed_password,
-        role=form.role.data
+        role=data["role"]
     )
 
 @user_bp.route("/change_data/<int:id>", methods=["GET", "POST"])
@@ -46,23 +62,42 @@ def edit_user_data(id):
         return redirect(url_for("home"))
     user = User.query.get_or_404(id)
     form = RegistrationForm(obj=user)
+    schema = UserSchema()
     if form.validate_on_submit():
+        user_data = {
+            "username": form.username.data,
+            "first_name": form.first_name.data,
+            "last_name": form.last_name.data,
+            "phone_number": form.phone_number.data,
+            "email": form.email.data,
+            "password": form.password.data,
+            "role": form.role.data
+        }
         try:
-            hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
-            user.username=form.username.data
-            user.first_name=form.first_name.data
-            user.last_name=form.last_name.data
-            user.phone_number=form.phone_number.data
-            user.email=form.email.data
-            user.password_hash=hashed_password
-            user.role=form.role.data
+            result = schema.load(user_data)
+            if user.username != result["username"]:
+                if User.query.filter_by(username=result["username"]).first():
+                    flash("Username is already in use, choose another.", "danger")
+                    return render_template("register.html", form=form, title="User Data Edit")
+            if user.email != result["email"]:
+                if User.query.filter_by(email=result["email"]).first():
+                    flash("Email is already in use, choose another.", "danger")
+                    return render_template("register.html", form=form, title="User Data Edit")
+            user.username = result["username"]
+            user.first_name = result["first_name"]
+            user.last_name = result["last_name"]
+            user.phone_number = result["phone_number"]
+            user.email = result["email"]
+            if form.password.data:
+                hashed_password = bcrypt.generate_password_hash(result["password"]).decode("utf-8")
+                user.password_hash = hashed_password
+            user.role = result["role"]
             db.session.commit()
             flash("Your user data updated successfully.", "success")
             return redirect(url_for("home"))
-        except IntegrityError as e:
-            db.session.rollback()
-            current_app.logger.exception(f"Registration error: {e}")
-            flash(f"Username or email in used, choose another", "danger")
+        except ValidationError as e:
+            current_app.logger.exception(f"Edit user data - validation error: {e}")
+            flash(f"Validation error: {e}", "danger")
         except Exception as e:
             db.session.rollback()
             current_app.logger.exception(f"Error during user data editing: {e}")
