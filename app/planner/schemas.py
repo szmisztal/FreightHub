32a38677e41901678910f2ee6935e33bd918fe4a
szmisztal <sz.misztal@gmail.com>
2024-06-01 12:45:36 +0200

    add schema for TransportationOrder model'

--- app/planner/schemas.py
+++ app/planner/schemas.py
@@ -13,3 +13,16 @@ class CompanySchema(Schema):
     def validate_phone_number(self, value):
         if not value.isdigit() and not (value.startswith('+') and value[1:].isdigit()):
             raise ValidationError("Phone number must contain only digits and optional leading '+'.")
+
+class PlannerTransportationOrderSchema(Schema):
+    creation_date = fields.Date(required=True)
+    created_by = fields.Int(required=True)
+    planned_delivery_date = fields.Date(required=True)
+    trailer_type = fields.Str(required=True, validate=validate.Length(max=16))
+    tractor_head = fields.Int(allow_none=True)
+    trailer = fields.Int(allow_none=True)
+    load_weight = fields.Int(required=True, validate=validate.Range(min=1, max=24000))
+    loading_place = fields.Int(required=True)
+    delivery_place = fields.Int(required=True)
+    driver = fields.Int(allow_none=True)
+    completed = fields.Bool(default=False)
