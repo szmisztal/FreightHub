7e46d9c17e0c7d9f74dd019fdba2d884fb366555
szmisztal <sz.misztal@gmail.com>
2024-06-04 16:15:49 +0200

    move validation from form to TransportationOrderSchema

--- app/common/schemas.py
+++ app/common/schemas.py
@@ -1,16 +1,17 @@
 from marshmallow import Schema, fields, validate, validates, ValidationError
 from datetime import date
+from .custom_utils import not_blank
 
 class TransportationOrderSchema(Schema):
     creation_date = fields.Date(required=True)
     created_by = fields.Int(required=True)
-    planned_delivery_date = fields.Date(required=True)
-    trailer_type = fields.Str(required=True, validate=validate.Length(max=16))
+    planned_delivery_date = fields.Date(required=True, validate=[not_blank])
+    trailer_type = fields.Str(required=True, validate=[validate.Length(max=16), not_blank])
     tractor_head = fields.Int(allow_none=True)
     trailer = fields.Int(allow_none=True)
-    load_weight = fields.Int(required=True, validate=validate.Range(min=1, max=24000))
-    loading_place = fields.Int(required=True)
-    delivery_place = fields.Int(required=True)
+    load_weight = fields.Int(required=True, validate=[validate.Range(min=1, max=24000), not_blank])
+    loading_place = fields.Int(required=True, validate=[not_blank])
+    delivery_place = fields.Int(required=True, validate=[not_blank])
     driver = fields.Int(allow_none=True)
     completed = fields.Bool(default=False)
 
